44ecbb302cf8510471683370c2e18a5fd9a47c58
nsduman <dumanlife@yandex.com>
2021-11-28 15:48:13 +0300

    Author.js is modified

--- src/components/Author.js
+++ src/components/Author.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function Author() {
   let params = useParams();
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    axios(
+      `https://my-json-server.typicode.com/dmitrijt9/book-api-mock/authors/${params.authorId}`
+    ).then((res) => setUser(res.data));
+  }, []);
+
   return (
     <div style={{ padding: "1rem" }}>
       <h2 style={{ margin: "0 1rem" }}>Author:{params.authorId}</h2>
-      <h3>Name:</h3>
-      <h3>Surname: </h3>
+      <h3>Name: {user.name} </h3>
+      <h3>Surname: {user.surname} </h3>
+      {console.log(params)}
     </div>
   );
 }
